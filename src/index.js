6e1060747a9c46f3eea9ecc21e6c0f6be596e526
Andrey Baryshev <chebb@yandex.ru>
2020-03-03 17:46:14 +0400

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let inputArr = expr.split('').filter(item => item == ' ' ? false : true);
+    let openBracket = 0;
+    let closeBracket = 0;
+    inputArr.forEach(el => {
+      if (el === '(') openBracket++;
+      if (el === ')') closeBracket++;
+    });
+    if (openBracket !== closeBracket) throw new Error('ExpressionError: Brackets must be paired.');
+    for (let i = 0; i < inputArr.length-1; i++) {
+      if( inputArr[i] == '/' && inputArr[i+1] == 0 ) throw new Error('TypeError: Division by zero.');
+    }
+    let getOutput = new Function('return ' + inputArr.join(''));
+    let output = getOutput();
+    return output;
 }
 
-module.exports = {
-    expressionCalculator
-}
+module.exports = {expressionCalculator}

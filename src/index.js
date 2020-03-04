715cf5a571977192d504e437bada484ba9c07648
Andrey Baryshev <chebb@yandex.ru>
2020-03-04 15:54:50 +0400

    fix: change solution to stack method

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    let inputArr = expr.split('').filter(item => item == ' ' ? false : true);
+    let inputArr = expr.split('').filter(item => item == ' ' || item == '' ? false : true);
+    for (let i = 0; i < inputArr.length-1; i++) {
+      if ( /\d+/.test(inputArr[i]) && /\d+/.test(inputArr[i+1]) ){
+        inputArr[i] += inputArr[i+1];
+        inputArr.splice(i+1, 1);
+        i--;
+      }
+    }
     let openBracket = 0;
     let closeBracket = 0;
     inputArr.forEach(el => {
@@ -15,9 +22,75 @@ function expressionCalculator(expr) {
     for (let i = 0; i < inputArr.length-1; i++) {
       if( inputArr[i] == '/' && inputArr[i+1] == 0 ) throw new Error('TypeError: Division by zero.');
     }
-    let getOutput = new Function('return ' + inputArr.join(''));
-    let output = getOutput();
-    return output;
+
+    let priority = {
+      '+': 1,
+      '-': 1,
+      '*': 2,
+      '/': 2,
+      '(': 0,
+      ')': 0
+    };
+    let numbersStack = [];
+    let operatorsStack = [];
+    for (let i = 0; i < inputArr.length; i++) {
+      if (/\d+/.test(inputArr[i])){
+        numbersStack.push(+inputArr[i]);
+        continue;
+      }
+      if (inputArr[i] == '('){
+        operatorsStack.push(inputArr[i]);
+        continue;
+      }
+      if (inputArr[i] == ')'){
+        while (operatorsStack[operatorsStack.length-1] !== '('){
+          calc();
+        }
+        operatorsStack.pop();
+        continue;
+      }
+      if ( priority[inputArr[i]] > priority[operatorsStack[operatorsStack.length-1]]){
+          operatorsStack.push(inputArr[i]);
+          continue;
+      }
+      while(priority[inputArr[i]] <= priority[operatorsStack[operatorsStack.length-1]] ){
+        calc();
+      }
+      operatorsStack.push(inputArr[i]);
+    }
+    while(operatorsStack.length > 0){
+      calc();
+    }
+
+    function calc (){
+      if (operatorsStack.length == 0) throw new Error ('Calc: not have operator!');
+      let operator = operatorsStack.pop();
+      let b = numbersStack.pop();
+      let a = numbersStack.pop();
+      let result;
+      switch (operator){
+        case '-':
+        result = a - b;
+        numbersStack.push(result);
+        break;
+
+        case '+':
+        result = a + b;
+        numbersStack.push(result);
+        break;
+
+        case '*':
+        result = a * b;
+        numbersStack.push(result);
+        break;
+
+        case '/':
+        result = a / b;
+        numbersStack.push(result);
+        break;
+      }
+    }
+    return numbersStack[0];
 }
 
 module.exports = {expressionCalculator}
